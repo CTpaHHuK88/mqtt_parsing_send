4f0a0dcdbcca3a55554d305d76e91d03ce3e3e5b
Rudolf <admin@admin.admin>
2025-06-18 12:08:23 +0300

    update_18.06.2025

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
 class ConfigPub():
-    MQTT_BROKER = "10.10.0.12"
+    MQTT_BROKER = "localhost"
     MQTT_TOPIC = "meteo/pub"
     MQTT_PORT = 1883
-    CH_HOST = "10.10.0.12"
+    CH_HOST = "localhost"
 
 class ConfigSub():
-<<<<<<< HEAD
-    MQTT_BROKER = ""
-    MQTT_TOPIC = ""
-    MQTT_PORT = 
-=======
-    MQTT_BROKER = "10.10.0.12"
+    MQTT_BROKER = "localhost"
     MQTT_TOPIC = "meteo/upload"
     MQTT_PORT = 1883
->>>>>>> 210ed039657be05f48083cc315239160ad110f28
